3048ecb673ec4280acfa9763eba006f8929c8e4e
alaaM <alaaMandour2024@gmail.com>
2025-12-24 22:08:45 +0200

    update on all method

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ port = 3000;
 // method (url,callback function hold posetional args (req,res,next)=>{})
 // -- get --
 app.get("/users",(req,res,next)=>{
-    // res.status(200).send("Hello Aloaaa") //send function can hold any type of masseges like >> test , json , html , etc
+    // res.status(200).send("Hello Aloaaa") //send function can hold any type of masseges like >> text , json , html , etc
     // res.status(200).send("<h1>Hello Aloaaa</h1>")
     // res.status(200).send({message:"Hello Aloaaa"})
 
@@ -30,7 +30,7 @@ app.delete("/users",(req,res,next)=>{
     res.status(200).json({message:"Delete User"}) 
 })
 
-// >>>>> to handel the error of not found url use  --> use method  <<<<
+// >>>>> to handel the error of not found url use  --> use OR all method <<<<
 //note ==> epress v4 put the url in this format -> "/*"  
 // but in express v5 --> "/*Ay 7haga" like "/*demo" , "/*hi", any thing after /*
 // and you can delete the url because use function works with any url by default
@@ -38,6 +38,12 @@ app.delete("/users",(req,res,next)=>{
 app.use("{/*demo}",(req,res,next)=>{
     res.status(404).json({message:"404 Oops ... Page Not Fount .."}) 
 })
+
+// >> OR all 👇
+
+// app.all("{/*demo}",(req,res,next)=>{
+//     res.status(404).json({message:"404 Oops ... Page Not Fount .."}) 
+// })
 
 //listn to the server to run it in a special port 
 app.listen(port,()=>{
